8b426ccb2442f353dee8cda2a593a85d5f9e7bbd
MTomasNievasM <marcelotomasnm@gmail.com>
2025-10-01 12:43:53 +0200

    cambios

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 window.onscroll = function() {scrollFunction()};
-
+//
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     document.getElementById("navbar").style.top = "0";
